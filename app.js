979244aa0d8fee811b1d598ca690c1dfc5a85535
Marc Wiesener <wiesener.marc@gmail.com>
2022-03-03 21:44:48 +0100

    sign up done

--- app.js
+++ app.js
@@ -2,36 +2,32 @@
 // https://www.npmjs.com/package/dotenv
 require("dotenv/config");
 
-// ℹ️ Connects to the database
+// Connects to the database
 require("./db");
 
 // Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require("express");
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
 const hbs = require("hbs");
-
-const app = express();
-
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
-
-// default value for title local
 const capitalized = require("./utils/capitalized");
 const projectName = "Hausparty";
 
+const app = express();
+
+// This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config")(app);
+
 app.locals.appTitle = `${capitalized(projectName)} `;
 
-// 👇 Start handling routes here
+// handling routes
 const index = require("./routes/index.routes");
-app.use("/", index);
-
 const partyRoutes = require("./routes/party.routes");
-app.use("/parties", partyRoutes);
+const authRoutes = require("./routes/auth.routes");
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+app.use("/", index);
+app.use("/parties", partyRoutes);
+app.use("/auth", authRoutes);
+
+// To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,71 +1,34 @@
-//! This is just a copy past from an older project. Feel free to delete or ignore.
-
+// {Importing third party Library}
 const router = require("express").Router();
-const bcryptjs = require("bcryptjs"); // import bcrypt
-const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+// {Relative imports - from within the project}
 const User = require("../models/User.model");
 
-const saltRound = 10;
-
-router.get("/login", (_, res) => {
-  res.render("auth/login");
-});
+const saltRounds = 10;
 
 router.get("/signup", (_, res) => {
   res.render("auth/signup");
 });
 
-router.post("/signup", (req, res) => {
-  const { username, password } = req.body;
-
-  bcryptjs
-    .genSalt(saltRound)
-    .then((salt) => bcryptjs.hash(password, salt))
-    .then((hash) => {
-      return User.create({
-        username,
-        password: hash,
-      });
-    })
-    .then((userFromDb) => {
-      res.redirect("/");
-    })
-    .catch((error) => {
-      if (error.code === 11000) {
-        res.status(400).render("auth/signup", {
-          errorMessage: error.message,
-        });
-      }
+router.post("/signup", async (req, res) => {
+  const { username, password, email } = req.body;
+  console.log(req.body);
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
     });
+    res.redirect("/user-profile");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  User.findOne({ username }).then((user) => {
-    if (!user) {
-      res.render("auth/login", {
-        errorMessage: "Username is registered",
-      });
-    } else if (bcryptjs.compareSync(password, user.password)) {
-      console.log(user);
-      req.session.currentUser = user;
-      res.redirect("/welcome");
-
-      //   res.render("/user/welcome", { user });
-    } else {
-      res.render("auth/login", {
-        errorMessage: "Incorrect credentials",
-      });
-    }
-  });
+router.get("/login", (_, res) => {
+  res.render("auth/login");
 });
 
-router.get("/logout", (req, res) => {
-  req.session.destroy((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.redirect("/auth/login");
-    }
-  });
-});
+module.exports = router;
