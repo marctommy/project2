b0662b564f979ccb0370396302c777ba36a60d89
Marc Wiesener <wiesener.marc@gmail.com>
2022-03-04 00:48:45 +0100

    login and signup and css bootstrap fixed

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,1 +1,12 @@
-//? Why do  I see this? spokfoaisdjfpo
+const router = require("express").Router();
+const User = require("../models/User.model");
+
+router.get("/", (_, res) => {
+  User.find()
+    .then((allUsers) => {
+      res.render("users/user-profile", { allUsers });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
