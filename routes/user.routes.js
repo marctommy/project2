309feb4315c6d2f4b4393311aa38f991c0ada93f
Marc Wiesener <wiesener.marc@gmail.com>
2022-03-05 10:42:05 +0100

    before pulling main

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,2 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
-
-router.get("/", (_, res) => {
-  User.find()
-    .then((allUsers) => {
-      res.render("users/user-profile", { allUsers });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
